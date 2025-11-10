552a76f97df5c7d9982923e10ef11fd124f5e781
Matthias Feist <matthias@foodfacts.se>
2025-11-10 21:16:41 +0100

    configure vite for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/switch/', // needed for the github pages deployment
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
