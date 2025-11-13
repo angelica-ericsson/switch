d0271282bef997621deabf34d7d9145df88c09ff
Matthias Feist <matthias@foodfacts.se>
2025-11-13 21:59:12 +0100

    created separate css for editor and game

--- src/editor.tsx
+++ src/editor.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './editor.css';
 import { Editor } from './features/editor/editor.tsx';
 
 createRoot(document.getElementById('root')!).render(
