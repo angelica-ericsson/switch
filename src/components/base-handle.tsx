69781966159ded05166de043fce3e9dec8b4903e
Matthias Feist <matthias@foodfacts.se>
2025-11-15 19:01:03 +0100

    made handles larger

--- src/components/base-handle.tsx
+++ src/components/base-handle.tsx
@@ -5,21 +5,19 @@ import { cn } from '@/lib/utils';
 
 export type BaseHandleProps = HandleProps;
 
-export const BaseHandle = forwardRef<HTMLDivElement, BaseHandleProps>(
-  ({ className, children, ...props }, ref) => {
-    return (
-      <Handle
-        ref={ref}
-        className={cn(
-          'min-h-4! min-w-4! rounded-full border border-gray-600! bg-white! transition dark:border-secondary dark:bg-secondary',
-          className,
-        )}
-        {...props}
-      >
-        {children}
-      </Handle>
-    );
-  },
-);
+export const BaseHandle = forwardRef<HTMLDivElement, BaseHandleProps>(({ className, children, ...props }, ref) => {
+  return (
+    <Handle
+      ref={ref}
+      className={cn(
+        'min-h-5! min-w-5! rounded-md border border-gray-600! bg-white! transition dark:border-secondary dark:bg-secondary',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </Handle>
+  );
+});
 
 BaseHandle.displayName = 'BaseHandle';
--- src/components/numbered-handle.tsx
+++ src/components/numbered-handle.tsx
@@ -7,21 +7,19 @@ export type NumberedHandleProps = HandleProps & {
   number: string;
 };
 
-export const NumberedHandle = forwardRef<HTMLDivElement, NumberedHandleProps>(
-  ({ className, number, ...props }, ref) => {
-    return (
-      <Handle
-        ref={ref}
-        className={cn(
-          'inline-flex items-center justify-center rounded-full px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 border border-gray-600! bg-white! transition dark:border-secondary dark:bg-secondary',
-          className,
-        )}
-        {...props}
-      >
-        {number}
-      </Handle>
-    );
-  },
-);
+export const NumberedHandle = forwardRef<HTMLDivElement, NumberedHandleProps>(({ className, number, ...props }, ref) => {
+  return (
+    <Handle
+      ref={ref}
+      className={cn(
+        'inline-flex items-center justify-center rounded-md px-2.5 py-0.5 text-md font-medium w-fit whitespace-nowrap shrink-0 border border-gray-600! bg-white! transition dark:border-secondary dark:bg-secondary',
+        className,
+      )}
+      {...props}
+    >
+      {number}
+    </Handle>
+  );
+});
 
 NumberedHandle.displayName = 'NumberedHandle';
