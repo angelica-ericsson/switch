7a62bcb5ca35c64de22b55e2e15ed2d1c12b2de0
Matthias Feist <matthias@foodfacts.se>
2025-11-25 08:24:08 +0100

    removed persistance of game state

--- src/features/game-engine/layout/gameLayout.tsx
+++ src/features/game-engine/layout/gameLayout.tsx
@@ -1,28 +1,8 @@
-import { useGameState } from '../state';
 import logoUrl from '../../../assets/logo.svg';
 import { StatusBar } from './statusBar';
 
 export function GameLayout(props: React.PropsWithChildren) {
-  const state = useGameState((state) => state);
-
-  return (
-    <>
-      {/* Main Content */}
-      <div className="flex min-h-[90vh] flex-1 flex-col items-center justify-center p-8 lg:flex-row">{props.children}</div>
-
-      {/* Debug Bar */}
-      <div className="flex gap-4 text-white">
-        {Object.entries(state).map((entry) => {
-          if (typeof entry[1] === 'function' || typeof entry[1] === 'object') return;
-          return (
-            <div key={entry[0]}>
-              {entry[0]}: {JSON.stringify(entry[1])}
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
+  return <div className="flex min-h-[90vh] flex-1 flex-col items-center justify-center p-8 lg:flex-row">{props.children}</div>;
 }
 
 export function GameBackground(props: React.PropsWithChildren) {
