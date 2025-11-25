675f851ac75a42900de5ec064bb268470e7482dd
Matthias Feist <matthias@foodfacts.se>
2025-11-25 14:29:50 +0100

    installed Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import './index.css';
 import './i18n';
+import * as Sentry from '@sentry/react';
 
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
@@ -7,6 +8,13 @@ import { RouterProvider, createRouter } from '@tanstack/react-router';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
+
+Sentry.init({
+  dsn: 'https://1f175da368255ed9713b2a8eacdd9c2c@o4510425877839872.ingest.de.sentry.io/4510425879347280',
+  integrations: [Sentry.replayIntegration()],
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 // Create a new router instance
 const router = createRouter({ routeTree, basepath: 'switch', defaultViewTransition: true });
