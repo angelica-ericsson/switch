79931ff5ee5db2d62da59b531c408b0831e201fd
Matthias Feist <matthias@foodfacts.se>
2025-12-06 15:09:41 +0100

    test to fix newspaper rendering

--- src/features/game-engine/screens/newsFlashScreen.tsx
+++ src/features/game-engine/screens/newsFlashScreen.tsx
@@ -35,7 +35,7 @@ export function NewsFlashScreen({ node }: NewsFlashScreenProps) {
 
   return (
     <GameLayout>
-      <div className="newspaper-bg max-w-5xl rotate-[0deg] p-12 font-serif md:rotate-2">
+      <div className="newspaper-bg max-w-5xl rotate-[0.01deg] p-12 font-serif md:rotate-2">
         <div className="newspaper-layer -z-3 -translate-x-1.5 -translate-y-3 -rotate-2 md:translate-y-1"></div>
         <div className="newspaper-layer -z-2 -translate-x-0.5 -translate-y-1.5 rotate-2 md:translate-y-0.5"></div>
         <div className="newspaper-layer -z-1"></div>
